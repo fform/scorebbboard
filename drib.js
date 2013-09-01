25fd687f168d4242620989660878d9fcd1dc82e6
Will Froelich <tw12lve@falsetruth.net>
2013-09-01 12:50:26 -0700

    init

---
+++ drib.js
@@ -0,0 +1,72 @@
+var request = require('request'),
+	rateLimit = require('function-rate-limit');
+
+exports.refreshList = function( list ){
+	console.log('Run list refresh [%s] - %s', list, new Date());
+	request.get('http://api.dribbble.com/shots/' + list +'?per_page=50', function(err, res){
+
+		var result = JSON.parse(res.body);
+
+		_(result.shots).each(function(shot){
+			connection.query("INSERT INTO `shots` SET ?", {
+				'id':shot.id,
+				'list': list,
+				'width': shot.width,
+				'height': shot.height,
+				'image_url': shot.image_url,
+				'title': shot.title,
+				'player_id': shot.player.id,
+				'player_likes': shot.player.likes_received_count,
+				'player_followers': shot.player.followers_count,
+				'player_following': shot.player.following_count,
+				'likes': shot.likes_count,
+				'views': shot.views_count,
+				'comments': shot.comments_count,
+				'created_at': new Date(Date.parse(shot.created_at)),
+			}, function(err,result){
+				if(!err){
+					connection.query("INSERT INTO `shots_status` SET ?", {
+						'id':shot.id,
+						'player_likes': shot.player.likes_received_count,
+						'player_followers': shot.player.followers_count,
+						'player_following': shot.player.following_count,
+						'likes': shot.likes_count,
+						'views': shot.views_count,
+						'comments': shot.comments_count,
+						'created_at': new Date(),
+					}, function(err,result){
+					});
+				}
+			});
+
+			
+		});
+	});
+};
+
+exports.refreshShots = function(){
+	console.log('Run shot refresh %s', new Date());
+	connection.query("SELECT * FROM `shots` ORDER BY created_at desc LIMIT 150", function(err, result){
+		var fn = rateLimit(30, 60*1000, function (shot) {
+			console.log('update shot %s %s', shot.title, new Date());
+			request.get('http://api.dribbble.com/shots/' + shot.id , function(err, res){
+
+				var thisShot = JSON.parse(res.body);
+				connection.query("INSERT INTO `shots_status` SET ?", {
+					'id':thisShot.id,
+					'player_likes': thisShot.player.likes_received_count,
+					'player_followers': thisShot.player.followers_count,
+					'player_following': thisShot.player.following_count,
+					'likes': thisShot.likes_count,
+					'views': thisShot.views_count,
+					'comments': thisShot.comments_count,
+					'created_at': new Date(),
+				}, function(err,result){
+				});
+			});
+		});
+		_(result).each(function(row){
+			fn(row);
+		});
+	});
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+var request = require('request'),
+	drib = require('./drib.js'),
+	rateLimit = require('function-rate-limit');
+
+global.mysql      = require('mysql');
+global._ = require('underscore');
+global.connection = mysql.createConnection({
+  host     : 'localhost',
+  database : 'drib',
+  user     : 'drib',
+  password : 'drib',
+});
+connection.connect();
+
+var setupFeed = function(delay, list){
+	console.log('Setup feed for %s', list);
+	setTimeout(function(){
+		setInterval(function(){
+			drib.refreshList(list);
+		}, 30 * 60 * 1000);
+		drib.refreshList(list);
+	}, delay * 60 * 1000);
+};
+
+setupFeed(0, 'debuts');
+setupFeed(5, 'everyone');
+setupFeed(15, 'popular');
+
+setTimeout(function(){
+
+	setInterval(function(){
+		drib.refreshShots();
+	}, 30 * 60 * 1000);
+
+}, 20 * 60 * 1000);
+
+drib.refreshShots();
